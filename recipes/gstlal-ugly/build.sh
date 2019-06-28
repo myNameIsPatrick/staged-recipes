5a5d68cd695421b53bc3f07d2b3a1d4e1b03fec4
Patrick Godwin <patrick.godwin@ligo.org>
2019-06-28 09:55:49 -0400

    update build.sh, meta.yaml with patches from duncanmmacleod

--- recipes/gstlal-ugly/build.sh
+++ recipes/gstlal-ugly/build.sh
@@ -3,6 +3,10 @@
 # conda-forge/conda-forge.github.io#621
 find ${PREFIX} -name "*.la" -delete
 
+# only link libraries we actually use
+export GSL_LIBS="-L${PREFIX}/lib -lgsl"
+export GSTLAL_LIBS="-L${PREFIX}/lib -lgstlal -lgstlaltags -lgstlaltypes"
+
 ./configure \
   --prefix=${PREFIX} \
   --without-doxygen \
